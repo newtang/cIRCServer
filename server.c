a82fc7eba1523493bb882e90de6b1d7c939d040f
Jon New <jonnew@gmail.com>
2016-12-08 15:23:10 -0500

    Adding attribution at the top for Beej's guide.

--- server.c
+++ server.c
@@ -1,3 +1,7 @@
+/**
+  * http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html#getaddrinfo
+  */
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -21,7 +25,16 @@ int main (){
 	int status;
 	struct addrinfo hints;
 	struct addrinfo *res;  // will point to the results
-	char s[INET6_ADDRSTRLEN];
+	char remoteIP[INET6_ADDRSTRLEN];
+
+	 struct sockaddr_storage remoteaddr; // client address
+
+	fd_set master;    // master file descriptor list
+    fd_set read_fds;  // temp file descriptor list for select()
+    int fdmax;        // maximum file descriptor number
+
+    FD_ZERO(&master);    // clear the master and temp sets
+    FD_ZERO(&read_fds);
 
 	memset(&hints, 0, sizeof hints); // make sure the struct is empty
 	hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
@@ -33,69 +46,79 @@ int main (){
 	    return 1;
 	}
 
-	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	int listener = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
 
 	// bind it to the port we passed in to getaddrinfo():
-	bind(sockfd, res->ai_addr, res->ai_addrlen);
+	bind(listener, res->ai_addr, res->ai_addrlen);
 
 	freeaddrinfo(res);
 
 	int queueSize = 20;
 
-	if( (status = listen(sockfd, queueSize)) != 0){
+	if( (status = listen(listener, queueSize)) != 0){
 		fprintf(stderr, "listen error: %s\n", gai_strerror(status));
 	    return 1;
 	}
+
+	fdmax = listener;
+	// add the listener to the master set
+    FD_SET(listener, &master);
+
 
 	// now accept an incoming connection:
 	struct sockaddr_storage their_addr;
     socklen_t addr_size = sizeof their_addr;
     while(1){
-
-    	//accept blocks
-	    int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-	    if (new_fd == -1) {
-            perror("accept");
-            continue;
+    	read_fds = master; // copy it
+    	printf("Awaiting connection or input\n");
+    	if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(4);
         }
 
-        inet_ntop(their_addr.ss_family,
-        	get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-        printf("server: got connection from %s\n", s);
+        for(int i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &read_fds)) { // we got one!!
+                if (i == listener) {
+                	int new_fd = accept(listener, (struct sockaddr *)&their_addr, &addr_size);
+                	if (new_fd != -1){
+                		FD_SET(new_fd, &master);
+                		if (new_fd > fdmax) {    // keep track of the max
+                            fdmax = new_fd;
+                        }
+                        printf("selectserver: new connection from %s on "
+                            "socket %d\n",
+                            inet_ntop(their_addr.ss_family,
+                                get_in_addr((struct sockaddr*)&their_addr),
+                                remoteIP, INET6_ADDRSTRLEN),
+                            new_fd);
+                	}
+                	else{
+                		perror("accept new connection");
+                	}
+                }
+                else {
+                	 int nbytes;
+                	 char buf[256];
 
-
-        /**
-          * This forks the program. It's essentially duplicated. Weirdly, this method essentially returns twice, one 
-          to its self, and one to it's child.  The child gets a return of '0' (so it enters this if statement)
-          and the original gets the pid of the child, so it loops again, and blocks on accept!
-
-        **/
-
-	    if (!fork()) { // this is the child process
-
-            close(sockfd); // child doesn't need the listener
-
-            char *msg = "Hello There!";
-			int len = strlen(msg);
-
-            if (send(new_fd, msg, len, 0) == -1)
-                perror("send");
-            
-            int bufLen = 200;
-            char buf[bufLen];
-            
-            while(recv(new_fd, buf, bufLen, 0)){
-            	printf("%s", buf);
-            	memset(buf, 0, bufLen);
+                	 if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+                	 	FD_CLR(i, &master);
+                	 }
+                	 else {
+                	 	//we received a message!
+                	 	printf("buf: %s", buf);
+                	 	for(int j=0; j<=fdmax; ++j){
+                	 		if (FD_ISSET(j, &master)){
+	                	 		if(j != listener && j != i){
+	                	 			send(j, buf, nbytes, 0);
+	                	 		}
+	                	 	}
+                	 	}
+                	}
+                }
             }
-            
-            close(new_fd);
-            return 0;
         }
-
-        close(new_fd);  // parent doesn't need this
-	}
+    }
 
 	return 0;	
 }
